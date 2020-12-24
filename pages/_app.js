9c6d3a96ff02ae3543fd25ad1c7d293f47d61e74
Vikram Pal <vikramsonupal@gmail.com>
2020-12-23 23:56:39 -0800

    Bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,40 @@
-import { ProvideAuth } from '../lib/auth';
+import { CSSReset } from '@chakra-ui/react';
+import { css, Global, ThemeProvider } from '@emotion/react';
+import { AuthProvider } from '../lib/auth';
+
+import theme from '../styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        style={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,37 @@
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+
 import { auth } from 'firebase';
+import { useAuth } from '../lib/auth';
+import { Button, Heading } from '@chakra-ui/react';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main className={styles.main}>
-        <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
-        {auth?.user && (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+    <div>
+      <main>
+        <Heading>Fast Feedback</Heading>
+        {auth.user ? (
+          <Button
+            colorScheme='red'
+            variant='solid'
+            fontWeight='medium'
+            onClick={(e) => auth.signout()}
+          >
+            Sign Out
+          </Button>
+        ) : (
+          <Button
+            colorScheme='green'
+            variant='solid'
+            fontWeight='medium'
+            onClick={(e) => auth.signinWithGithub()}
+          >
+            Sign In
+          </Button>
         )}
         <div>{auth?.user?.email}</div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
